d16b10a3a779a9c0baa3ff528468fd4442c1fe28
sumitvarun <sumitvarun0781@gmail.com>
2020-09-21 04:27:50 +0530

    a bmi calculator app made in flutter

---
+++ lib/components/reusablecard.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Reusablecard extends StatelessWidget {
+  Reusablecard({this.colour, this.cardchild, this.onPress});
+  final Color colour;
+  final Widget cardchild;
+  final Function onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: cardchild,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+const bottomContainerHeight = 80.0;
+const kActiveCardColour = Color(0xff1d1e33);
+const kInActiveCardColour = Color(0xFF111328);
+const kBottomColor = Color(0xFFEB1555);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 14.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 40.0,
+  fontWeight: FontWeight.w900,
+);
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+const kResultTextStyle = TextStyle(
+  color: Color(0xff24d876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
